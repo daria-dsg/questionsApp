20cd90ff7cc9ec5bb2d4cac4e563655c15797edf
Daria <daria.ui.ux@gmail.com>
2023-11-17 22:11:24 +0100

    implement user class

--- questions.rb
+++ questions.rb
@@ -1,6 +1,7 @@
 require 'singleton'
+require 'sqlite3'
 
-class QuestionsDatabase < SQLite3:Database
+class QuestionsDatabase < SQLite3::Database
 	include Singleton
 
   def initialize
@@ -8,5 +9,27 @@ class QuestionsDatabase < SQLite3:Database
     self.type_translation = true
     self.results_as_hash = true 
   end
+end
+
+class User
+	def self.find_by_id(id)
+		# user stores an array of hash with row as a key and values
+		user = QuestionsDatabase.instance.execute(<<-SQL, id)
+			SELECT 
+				*
+			FROM
+				users
+			WHERE 
+			  id = ?
+		SQL
+
+		User.new(user.first)
+	end
+
+	def initialize(options)
+		@id = options['id']
+		@fname = options['fname']
+		@lname = options['lname']
+	end
 end
 
