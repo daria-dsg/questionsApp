b570885fa648214e4532ff080b7b8e47e602a79b
Daria <daria.ui.ux@gmail.com>
2023-11-18 15:14:45 +0100

    add func to find user by name

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,10 @@
 require_relative 'questions_db'
 
 class User
+  attr_accessor :id, :fname, :lname
+
 	def self.find_by_id(id)
-		# user stores an array of hash with row as a key and values
+		# var stores an array of hash with row as a key and values
 		user = QuestionsDatabase.instance.execute(<<-SQL, id)
 			SELECT 
 				*
@@ -12,9 +14,26 @@ class User
 			  id = ?
 		SQL
 
+		raise "user with id #{id} is not in database" if user.empty?
 		User.new(user.first)
 	end
 
+	def self.find_by_name(fname, lname)
+		# var stores an array of hash with row as a key and values
+		user = QuestionsDatabase.instance.execute(<<-SQL, fname: fname, lname: lname)
+			SELECT 
+				*
+			FROM
+				users
+			WHERE 
+			  users.fname = :fname AND users.lname = :lname
+		SQL
+
+		raise "user with a name #{fname} #{lname} is not in database" if user.empty?
+		User.new(user.first)
+	end
+    
+  # options is a hash of attributes
 	def initialize(options)
 		@id = options['id']
 		@fname = options['fname']
