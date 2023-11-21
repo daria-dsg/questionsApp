1d671fc457850261131708fdb798a32798ed4300
Daria <daria.ui.ux@gmail.com>
2023-11-21 08:43:52 +0100

    add func to find followers with question id

--- question_follow.rb
+++ question_follow.rb
@@ -1,4 +1,5 @@
 require_relative 'questions_db'
+require_relative 'user'
 
 class QuestionFollow
   attr_accessor :id, :question_id, :user_id
@@ -16,8 +17,19 @@ class QuestionFollow
 
 		QuestionFollow.new(follow.first)
 	end
+
+  def self.followers_for_question_id(question_id)
+    users = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT users.id, users.fname, users.lname FROM users
+      INNER JOIN questions_follows
+      ON users.id = questions_follows.user_id
+      WHERE questions_follows.question_id = ?
+    SQL
+
+    users.map {|options| User.new(options)}
+  end
     
-    # options is a hash of attributes
+  # options is a hash of attributes
 	def initialize(options)
 		@id = options['id']
 		@question_id = options['question_id']
