760a455aa1cf11db3203f5e79426e3b0ccdfdb41
Daria <daria.ui.ux@gmail.com>
2023-11-21 07:07:38 +0100

    add save and child/parent func

--- reply.rb
+++ reply.rb
@@ -1,7 +1,8 @@
 require_relative 'questions_db'
+require_relative 'user'
 
 class Reply
-	attr_accessor :id, :user_id, :question_id, :body
+	attr_accessor :id, :user_id, :question_id, :parent_id, :body
 
 	def self.find_by_id(id)
 		# var stores an array of hash with row as a key and values
@@ -51,6 +52,7 @@ class Reply
 		@id = options['id']
     @user_id = options['user_id']
     @question_id = options['question_id']
+    @parent_id = options['parent_id']
 		@body = options['body']
 	end
 
@@ -58,21 +60,57 @@ class Reply
 		is_saved = self.id == nil ? false : true
 
 		unless is_saved
-			QuestionsDatabase.instance.execute(<<-SQL, self.user_id, self.question_id, self.body)
-				INSERT INTO replies(user_id, question_id, body)
-				VALUES (?, ?, ?)
+			QuestionsDatabase.instance.execute(<<-SQL, self.user_id, self.question_id, self.parent_id, self.body)
+				INSERT INTO replies(user_id, question_id, parent_id, body)
+				VALUES (?, ?, ?, ?)
 			SQL
 
     	self.id = QuestionsDatabase.instance.last_insert_row_id
 		else
-			QuestionsDatabase.instance.execute(<<-SQL, self.user_id, self.question_id, self.body, self.id)
+			QuestionsDatabase.instance.execute(<<-SQL, self.user_id, self.question_id, self.parent_id, self.body, self.id)
 				UPDATE
 				 replies
 				SET
-					user_id = ?, question_id = ?, body = ?
+					user_id = ?, question_id = ?, parent_id = ?, body = ?
 				WHERE
 				 replies.id = ?
 			SQL
 		end
 	end
+
+  def author
+		User.find_by_id(self.user_id)
+	end
+
+  def question
+		Question.find_by_id(self.question_id)
+	end
+
+  def parent_reply
+    reply = QuestionsDatabase.instance.execute(<<-SQL, self.parent_id)
+      SELECT 
+        *
+      FROM
+        replies
+      WHERE 
+        id = ?
+      SQL
+  
+    raise "reply with id #{id} does not have parent reply" if reply.empty?
+    Reply.new(reply.first)
+  end
+
+  def child_replies
+    reply = QuestionsDatabase.instance.execute(<<-SQL, self.id)
+      SELECT 
+        *
+      FROM
+        replies
+      WHERE 
+        replies.parent_id = ? 
+      SQL
+  
+    raise "reply with id #{id} does not have child replies" if reply.empty?
+    Reply.new(reply.first)
+  end
 end
