7f59370d6354209397ef4cbad4377d6beab65ec8
Daria <daria.ui.ux@gmail.com>
2023-11-23 07:41:47 +0100

    fix indentation  issue

--- question_follow.rb
+++ question_follow.rb
@@ -4,19 +4,19 @@ require_relative 'user'
 class QuestionFollow
   attr_accessor :id, :question_id, :user_id
     
-	def self.find_by_id(id)
-		# var stores an array of hash with row as a key and values
-		follow = QuestionsDatabase.instance.execute(<<-SQL, id)
-			SELECT 
-				*
-			FROM
-				questions_follows
-			WHERE 
-			  id = ?
-		SQL
+  def self.find_by_id(id)
+    # var stores an array of hash with row as a key and values
+    follow = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT 
+        *
+      FROM
+        questions_follows
+      WHERE 
+        id = ?
+    SQL
 
-		QuestionFollow.new(follow.first)
-	end
+    QuestionFollow.new(follow.first)
+  end
 
   def self.followers_for_question_id(question_id)
     users = QuestionsDatabase.instance.execute(<<-SQL, question_id)
@@ -41,9 +41,9 @@ class QuestionFollow
   end
     
   # options is a hash of attributes
-	def initialize(options)
-		@id = options['id']
-		@question_id = options['question_id']
-		@user_id = options['user_id']
-	end
+  def initialize(options)
+    @id = options['id']
+    @question_id = options['question_id']
+    @user_id = options['user_id']
+  end
 end
