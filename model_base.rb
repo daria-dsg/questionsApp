c02db28f987bba9e105a1c1eed5ddb2a533f60fd
Daria <daria.ui.ux@gmail.com>
2023-11-27 08:24:44 +0100

    add func find by id

---
+++ model_base.rb
@@ -0,0 +1,23 @@
+require 'active_support/inflector'
+
+class ModelBase
+
+  # return name of the class as a string in plural
+  def self.table
+    self.to_s.tableize
+  end
+
+  def self.find_by_id(id)
+    # var stores an array of hash with row as a key and values
+    data = QuestionsDatabase.instance.execute(<<-SQL, id: id)
+      SELECT 
+        *
+      FROM
+        #{table}
+      WHERE 
+        id = :id
+    SQL
+
+    data.nil? ? nil : self.new(data.first)
+  end
+end
